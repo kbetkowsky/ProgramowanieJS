3e01385ea1a92f56a670d7472c2ad6dd98528972
kbetkowsky <92058076+kbetkowsky@users.noreply.github.com>
2023-03-18 00:45:57 +0100

    Update index.js

--- assync-add/index.js
+++ assync-add/index.js
@@ -6,19 +6,28 @@ async function asyncAdd(...args) {
 	return sum
 }
 
-async function measureTime() {
+function measureTime(func, args) {
 	const start = performance.now()
-	const nums = Array.from({ length: 100 }, () =>
-		Math.floor(Math.random() * 100)
-	)
-	const result = await asyncAdd(...nums)
-
-	const end = performance.now()
-	const timeElapsed = end - start
-
-	console.log(`Time elapsed: ${timeElapsed.toFixed(2)} ms`)
-	console.log(`Number of async operations: ${nums.length}`)
-	console.log(`Result: ${result}`)
+	const result = func(...args).then(result => {
+		const end = performance.now()
+		const timeElapsed = end - start
+		console.log(`Time elapsed: ${timeElapsed.toFixed(2)} ms`)
+		console.log(`Number of async operations: ${args.length}`)
+		console.log(`Result: ${result}`)
+	})
 }
 
-measureTime()
+async function addNumbers(...nums) {
+	const promises = []
+	for (let i = 0; i < nums.length; i += 100) {
+		promises.push(asyncAdd(...nums.slice(i, i + 100)))
+	}
+	const results = await Promise.all(promises)
+	return results.reduce((acc, cur) => acc + cur, 0)
+}
+
+const nums = Array.from({ length: 10000 }, () =>
+	Math.floor(Math.random() * 100)
+)
+
+measureTime(addNumbers, nums)
